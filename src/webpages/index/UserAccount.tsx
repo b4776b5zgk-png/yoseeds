2135ee80804cbd079f0e2891a04f7bd7af6998a4
yoseeds <b4776b5zgk@privaterelay.appleid.com>
2026-07-16 14:04:02 +0300

    Create UserAccount.tsx

---
+++ src/webpages/index/UserAccount.tsx
@@ -0,0 +1,18 @@
+export default function UserAccount() {
+  return (
+    <div>
+      <h1>👤 My YoSeeds Account</h1>
+
+      <p>
+        Welcome to your account.
+      </p>
+
+      <ul>
+        <li>View orders</li>
+        <li>Manage profile</li>
+        <li>Track purchases</li>
+      </ul>
+
+    </div>
+  );
+}
