da8cef3317864b6d61f7d001958e35869cd096b4
yoseeds <b4776b5zgk@privaterelay.appleid.com>
2026-07-16 13:58:46 +0300

    Create FarmerAdmin.tsx

---
+++ src/webpages/index/FarmerAdmin.tsx
@@ -0,0 +1,18 @@
+export default function FarmerAdmin() {
+  return (
+    <div>
+      <h1>🌾 Farmer Admin Dashboard</h1>
+
+      <p>
+        Manage YoSeeds farming products.
+      </p>
+
+      <ul>
+        <li>Add seeds</li>
+        <li>Update plants</li>
+        <li>Manage farming information</li>
+      </ul>
+
+    </div>
+  );
+}
