1fe05ca33f4f732b99e59c9849d6ff0c7a8978c6
yoseeds <b4776b5zgk@privaterelay.appleid.com>
2026-07-16 13:31:33 +0300

    Update superadmin.tsx

--- src/webpages/index/superadmin.tsx
+++ src/webpages/index/superadmin.tsx
@@ -1,1 +1,20 @@
+export default function SuperAdmin() {
+  return (
+    <div>
+      <h1>👑 Super Admin Dashboard</h1>
 
+      <p>
+        Welcome Super Admin.
+        You have full control of YoSeeds.
+      </p>
+
+      <ul>
+        <li>Manage users</li>
+        <li>Manage admins</li>
+        <li>Manage products</li>
+        <li>View reports</li>
+      </ul>
+
+    </div>
+  );
+}
